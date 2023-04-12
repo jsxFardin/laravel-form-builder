4a1156cd5eee298dd2b5ffd4dbbb4ca3c9e8a4e2
Fardin Ahsan <fardin.ahsan75@gmail.com>
2023-04-12 23:19:32 +0600

    first commit

---
+++ src/Providers/FormBuilderProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Jsxfardin\LaravelFormBuilder\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class FormBuilderProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/../config/formbuilder.php' => config_path('formbuilder.php'),
+        ], 'config');
+    }
+
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__ . '/../config/formbuilder.php',
+            'formbuilder'
+        );
+    }
+}
---
+++ src/config/formbuilder.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'database' => [
+        'name' => env('DB_DATABASE', 'form_builder'),
+        'user' => env('DB_DATABASE', 'root'),
+        'password' => env('DB_DATABASE', 'password'),
+    ]
+];
